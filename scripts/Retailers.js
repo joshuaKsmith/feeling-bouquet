58e007ad2375ca9df3f0f84d79d80239e9f9c337
unknown <mtgmirage@protonmail.com>
2024-10-07 11:50:39 -0500

    refactored retailers.js

--- scripts/Retailers.js
+++ scripts/Retailers.js
@@ -11,40 +11,44 @@ export const RetailerOptions = async () => {
 
     let retailersHTML = "<ul id='retailers-list'>"
 
+    // make a list item for each retailer
     retailersHTML += retailers.map((retailer) => {
+        // begin html construction
         let html = `<li class="retailer" value="${retailer.id}">
             ${retailer.name} -- purchases from ${retailer.distributor.name}, who purchases from 
         `
-        const distStringArray = nurseryDistributions.filter((nurseryDistribution) => 
-            nurseryDistribution.distributorId === retailer.distributorId
-        )
         
-        const nurseryArray = distStringArray.map((nurseryDistribution) => {
+        // populate array of nursery objects that share a distributorId with the current retailer
+        const nurseryArray = nurseryDistributions.filter((nurseryDistribution) => 
+            nurseryDistribution.distributorId === retailer.distributorId
+        ).map((nurseryDistribution) => {
             return nurseryDistribution.nursery
         })
-    
+        
+        // use a set so we don't repeat flower names if a distributor gets the same flower type from more than 1 nursery
         const flowerStringSet = new Set()
 
+
         html += nurseryArray.map((nursery) => {
+            // add flower name to our set if a flowerDisplay has the nurseryId of the current nursery 
             for (const flowerDisplay of flowerDisplays) {
                 if (flowerDisplay.nurseryId === nursery.id) {
                     flowerStringSet.add(flowerDisplay.flower.commonName)
                 }
             }
+            // add nursery names to html construction
             return nursery.name
         }).join(" and ")
     
         html += ". So, the flower types available here are: "
-    
-        const flowerStringArray = [...flowerStringSet]
-        html += flowerStringArray.join(", and ")
+        
+        // convert our set to an array
+        html += [...flowerStringSet].join(", and ")
 
         html += "</li>"
         return html
         
     }).join("")
-        
-    
 
     retailersHTML += "</ul>"
     return retailersHTML
